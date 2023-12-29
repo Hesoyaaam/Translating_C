151ea4fe088a07261f66b43e362266217ac546d3
AJI <ajisejati@01gmail.com>
2023-12-29 11:20:37 +0700

    first commit

---
+++ TRANSLATING2/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+namespace TRANSLATING2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnParsing = new System.Windows.Forms.Button();
+            this.btnVizualize = new System.Windows.Forms.Button();
+            this.btnSimulate = new System.Windows.Forms.Button();
+            this.btnCCODE = new System.Windows.Forms.Button();
+            this.btnCJSON = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(609, 75);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox2.Size = new System.Drawing.Size(483, 526);
+            this.richTextBox2.TabIndex = 14;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(120, 76);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(483, 525);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(14, 304);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(100, 40);
+            this.btnHitung.TabIndex = 12;
+            this.btnHitung.Text = "Translate";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(14, 76);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(100, 40);
+            this.btnUpload.TabIndex = 11;
+            this.btnUpload.Text = "Browse";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(14, 561);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 40);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(332, 617);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(100, 40);
+            this.btnExport.TabIndex = 16;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(14, 617);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(100, 40);
+            this.btnHelp.TabIndex = 17;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // btnParsing
+            // 
+            this.btnParsing.Location = new System.Drawing.Point(14, 131);
+            this.btnParsing.Name = "btnParsing";
+            this.btnParsing.Size = new System.Drawing.Size(100, 40);
+            this.btnParsing.TabIndex = 18;
+            this.btnParsing.Text = "Parsing";
+            this.btnParsing.UseVisualStyleBackColor = true;
+            // 
+            // btnVizualize
+            // 
+            this.btnVizualize.Location = new System.Drawing.Point(14, 246);
+            this.btnVizualize.Name = "btnVizualize";
+            this.btnVizualize.Size = new System.Drawing.Size(100, 40);
+            this.btnVizualize.TabIndex = 19;
+            this.btnVizualize.Text = "Visualize";
+            this.btnVizualize.UseVisualStyleBackColor = true;
+            // 
+            // btnSimulate
+            // 
+            this.btnSimulate.Location = new System.Drawing.Point(14, 187);
+            this.btnSimulate.Name = "btnSimulate";
+            this.btnSimulate.Size = new System.Drawing.Size(100, 40);
+            this.btnSimulate.TabIndex = 20;
+            this.btnSimulate.Text = "Simulate";
+            this.btnSimulate.UseVisualStyleBackColor = true;
+            // 
+            // btnCCODE
+            // 
+            this.btnCCODE.Location = new System.Drawing.Point(226, 617);
+            this.btnCCODE.Name = "btnCCODE";
+            this.btnCCODE.Size = new System.Drawing.Size(100, 40);
+            this.btnCCODE.TabIndex = 21;
+            this.btnCCODE.Text = "Copy Code";
+            this.btnCCODE.UseVisualStyleBackColor = true;
+            this.btnCCODE.Click += new System.EventHandler(this.btnCCODE_Click);
+            // 
+            // btnCJSON
+            // 
+            this.btnCJSON.Location = new System.Drawing.Point(120, 617);
+            this.btnCJSON.Name = "btnCJSON";
+            this.btnCJSON.Size = new System.Drawing.Size(100, 40);
+            this.btnCJSON.TabIndex = 22;
+            this.btnCJSON.Text = "Copy JSON";
+            this.btnCJSON.UseVisualStyleBackColor = true;
+            this.btnCJSON.Click += new System.EventHandler(this.btnCJSON_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(448, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 25);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "From xtUML JSOM MODEL to C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 667);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCJSON);
+            this.Controls.Add(this.btnCCODE);
+            this.Controls.Add(this.btnSimulate);
+            this.Controls.Add(this.btnVizualize);
+            this.Controls.Add(this.btnParsing);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnParsing;
+        private System.Windows.Forms.Button btnVizualize;
+        private System.Windows.Forms.Button btnSimulate;
+        private System.Windows.Forms.Button btnCCODE;
+        private System.Windows.Forms.Button btnCJSON;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TRANSLATING2/Form1.cs
@@ -0,0 +1,353 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using static TRANSLATING2.Form1.JsonData;
+
+namespace TRANSLATING2
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder sourceCodeBuilder;
+        private string selectedJsonFilePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Title = "Open Json Diagram File",
+                Filter = "Json Diagram Files|*.json"
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedJsonFilePath = dialog.FileName;
+                string displayJson = File.ReadAllText(selectedJsonFilePath);
+                richTextBox1.Text = displayJson;
+            }
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            selectedJsonFilePath = null; // Clear the selected JSON file path
+        }
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(selectedJsonFilePath) && File.Exists(selectedJsonFilePath))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "C Files|*.c";
+                saveFileDialog.Title = "Save C File";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+
+                    try
+                    {
+                        using (StreamWriter sw = new StreamWriter(filePath))
+                        {
+                            // Write the generated C code to the file
+                            sw.Write(richTextBox2.Text);
+                        }
+
+                        MessageBox.Show("File successfully saved!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please generate C code before exporting.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(selectedJsonFilePath) && File.Exists(selectedJsonFilePath))
+                {
+                    sourceCodeBuilder = new StringBuilder();
+                    GenerateCCode(selectedJsonFilePath);
+                    richTextBox2.Text = sourceCodeBuilder.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a valid JSON file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error generating C code: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            string helpMessage = "This Application is to generate file JSON to C programming language.\n\n" +
+
+                         "1. Click 'Browse' to select a file JSON.\n" +
+                         "2. Click 'Parsing' to check JSON data\n"+
+                         "3. Click 'Simulate' to simulation this program\n"+
+                         "4. Click Visualize to visualization all data\n" +
+                         "5. Click 'Translate' to translate file JSON to C programming language.\n" +
+                         "6. Click 'Clear' to clear all data that been upload and translate\n" +
+                         "7. Click 'Copy Json' to copy JSON data in textbox\n" +
+                         "8. Click 'Copy Code' to copy C code in textbox\n"+
+                         "9. Click 'Export' to save the translated data in a C programming language file";
+                         
+
+            MessageBox.Show(helpMessage, "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void GenerateCCode(string jsonFilePath)
+        {
+            try
+            {
+                string umlDiagramJson = File.ReadAllText(jsonFilePath);
+                JsonData json = JsonConvert.DeserializeObject<JsonData>(umlDiagramJson);
+ 
+                GenerateStructsAndAssociations(json);
+
+                CloseNamespace();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error parsing JSON: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void GenerateStructsAndAssociations(JsonData json)
+        {
+            GenerateNamespace(json.sub_name);
+
+            foreach (var model in json.model)
+            {
+                switch (model.type)
+                {
+                    case "class":
+                        GenerateClassStruct(model);
+                        break;
+                    case "association":
+                        GenerateAssociationStruct(model);
+                        break;
+                    // Tambahkan kasus lainnya sesuai kebutuhan
+                }
+            }
+        }
+
+        private void GenerateNamespace(string namespaceName)
+        {
+            sourceCodeBuilder.AppendLine("#include <stdio.h>");
+            sourceCodeBuilder.AppendLine("#include <string.h>"); 
+            sourceCodeBuilder.AppendLine($"namespace {namespaceName}");
+            sourceCodeBuilder.AppendLine("{");
+        }
+        private void GenerateClassStruct(JsonData.Model classModel)
+        {
+            string className = classModel.class_name;
+
+            sourceCodeBuilder.AppendLine($"struct {className} {{");
+
+            foreach (var attribute in classModel.attributes)
+            {
+                string attributeType = MapDataType(attribute.data_type.ToLower());
+                sourceCodeBuilder.AppendLine($"    {attributeType} {attribute.attribute_name}; ");
+            }
+
+            sourceCodeBuilder.AppendLine("};");
+            sourceCodeBuilder.AppendLine();
+        }
+        private void GenerateAssociationStruct(JsonData.Model associationModel)
+        {
+            string associationName = associationModel.name;
+
+            sourceCodeBuilder.AppendLine($"struct {associationName} {{");
+
+            // Assuming there are two classes in the association (Mahasiswa and Matakuliah)
+            if (associationModel.@class.Count == 2)
+            {
+                var class1 = associationModel.@class[0];
+                var class2 = associationModel.@class[1];
+
+                string class1Type = MapDataType(class1.class_name.ToLower());
+                string class2Type = MapDataType(class2.class_name.ToLower());
+
+                string multiplicity1 = class1.class_multiplicity;
+                string multiplicity2 = class2.class_multiplicity;
+
+                // Handle the case when both classes have multiplicity "1..1"
+                if (multiplicity1 == "1..1" && multiplicity2 == "1..1")
+                {
+                    sourceCodeBuilder.AppendLine($"    struct {class1Type} {class1.class_name};");
+                    sourceCodeBuilder.AppendLine($"    struct {class2Type} {class2.class_name};");
+                }
+                else
+                {
+                    sourceCodeBuilder.AppendLine($"    struct {class1Type} {class1.class_name} {class2Type} {class2.class_name};");
+                }
+            }
+
+            sourceCodeBuilder.AppendLine("};");
+            sourceCodeBuilder.AppendLine();
+            if (associationModel.model != null)
+            {
+                GenerateAssociationClassStruct(associationModel.model);
+            }
+        }
+        private void GenerateAssociationClassStruct(JsonData.Model associationClassModel)
+        {
+            string classType = MapDataType(associationClassModel.class_name.ToLower());
+
+            sourceCodeBuilder.AppendLine($"struct {associationClassModel.class_name} {{");
+
+            foreach (var attribute in associationClassModel.attributes)
+            {
+                string attributeType = MapDataType(attribute.data_type.ToLower());
+                sourceCodeBuilder.AppendLine($"    {attributeType} {attribute.attribute_name};");
+            }
+
+            sourceCodeBuilder.AppendLine("};");
+            sourceCodeBuilder.AppendLine();
+
+        }
+        private void CloseNamespace()
+        {
+            sourceCodeBuilder.AppendLine("}");
+            sourceCodeBuilder.AppendLine();
+        }
+        private string MapDataType(string dataType)
+        {
+            if (dataType == null)
+            {
+                return "UNKNOWN_TYPE";
+            }
+
+            switch (dataType.ToLower())
+            {
+                case "id":
+                    return "int";
+                case "state":
+                    return "char";
+                case "string":
+                    return "char[]";
+                case "integer":
+                    return "int";
+                case "real":
+                    return "float";
+                // Add more mappings as needed
+                default:
+                    return dataType; // For unknown types, just pass through
+            }
+        }
+
+        public class JsonData
+        {
+            public string type { get; set; }
+            public string sub_id { get; set; }
+            public string sub_name { get; set; }
+            public List<Model> model { get; set; }
+
+            public class Model
+            {
+                public string type { get; set; }
+                public string class_id { get; set; }
+                public string class_name { get; set; }
+                public string KL { get; set; }
+                public string name { get; set; }
+                public List<Attribute1> attributes { get; set; }
+                public List<State> states { get; set; }
+                public Model model { get; set; }
+                public List<Class1> @class { get; set; }
+            }
+            public class State
+            {
+                public string state_id { get; set; }
+                public string state_name { get; set; }
+                public string state_value { get; set; }
+                public string state_type { get; set; }
+                public string[] state_event { get; set; }
+                public string[] state_function { get; set; }
+            }
+            public class Attribute1
+            {
+                public string attribute_name { get; set; }
+                public string data_type { get; set; }
+                public string default_value { get; set; }
+                public string attribute_type { get; set; }
+            }
+
+            public class Class1
+            {
+                public string class_name { get; set; }
+                public string class_multiplicity { get; set; }
+                public List<Attribute> attributes { get; set; }
+                public string type { get; internal set; }
+            }
+
+            public class Attribute
+            {
+                public string attribute_name { get; set; }
+                public string data_type { get; set; }
+                public string attribute_type { get; set; }
+                public string default_value { get; set; }
+            }
+        }
+
+        private void btnCJSON_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(selectedJsonFilePath) && File.Exists(selectedJsonFilePath))
+            {
+                try
+                {
+                    // Read the JSON file content
+                    string jsonContent = File.ReadAllText(selectedJsonFilePath);
+
+                    // Copy the JSON content to the clipboard
+                    Clipboard.SetText(jsonContent);
+
+                    MessageBox.Show("JSON content copied to clipboard.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error copying JSON content: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid JSON file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCCODE_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(richTextBox2.Text))
+            {
+                try
+                {
+                    // Copy the generated code to the clipboard
+                    Clipboard.SetText(richTextBox2.Text);
+
+                    MessageBox.Show("Generated code copied to clipboard.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error copying generated code: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please generate C code before copying.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
